7960d5eba9a92c31a8a6ecde5fd33776dd26efae
Roi Mekaiton <roimekaiton@gmail.com>
2024-01-07 19:21:26 +0200

    basic autonoumus functions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FuckV2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FuckV2.java
@@ -9,7 +9,6 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 // import com.qualcomm.robotcore.util.Range;
 import java.lang.Math;
 
-
 @TeleOp(name="FuckV2", group="Linear Opmode")
 // @Disabled
 public class FuckV2 extends LinearOpMode {
